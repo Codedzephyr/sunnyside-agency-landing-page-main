f6157139f2565e79c356d1b854d8e8583cc123c6
Codedzephyr <59132499+Codedzephyr@users.noreply.github.com>
2021-08-05 19:19:03 +0100

    messages

---
+++ main.js
@@ -0,0 +1,7 @@
+const NavigationLinks = document.getElementById("navigation");
+const Hamburger = document.getElementById("nav-bar");
+
+Hamburger.addEventListener("click", () => {
+  NavigationLinks.classList.toggle("nav-links-container");
+  console.log(NavigationLinks);
+});
